48bbf6e605b2f7fee748da1ebaeb360a3801d662
bag3lk <czemujalol@outlook.com>
2025-04-03 11:39:28 +0200

    naprawa

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+  <div>
+  <ul>
+    <li *ngFor="let task of tasks">{{ task }}</li>
+  </ul>
+</div>
+
+
   `,
 })
 export class DemoComponent {
+  tablica: string[] = ['', ''];
 }
 
