a3b01f4382ae7a66163c59522d16b18783bc01b9
bag3lk <czemujalol@outlook.com>
2025-04-03 10:46:47 +0200

    coscoscos

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {bootstrapApplication} from '@angular/platform-browser';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+@Component({
+  selector: 'app-root',
+  template: `
+    <label for="name">Name:</label>
+    <input type="text" id="name" [(ngModel)]="name" placeholder="Enter a name here" />
+    <hr />
+    <h1>Hello {{ name }}!</h1>
+  `,
+  imports: [FormsModule],
+})
+export class DemoComponent {
+  name = '';
+}
+
+bootstrapApplication(DemoComponent);
