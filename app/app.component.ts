edb70984b94ce4f07fdf68019e8436c651ca8815
bag3lk <czemujalol@outlook.com>
2025-04-04 11:02:15 +0200

    t1

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +6,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'nowe';
+  title = 'Lista';
 }
---
+++ app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';  // Importing the NgModule decorator
+import { BrowserModule } from '@angular/platform-browser';  // Required for running Angular in the browser
+import { AppComponent } from './app.component';  // Importing the root component
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
+
+//cotojest
