90004a4221d60e5f42bc92a349a3583ea6d84b86
bag3lk <czemujalol@outlook.com>
2025-04-04 11:52:38 +0200

    działająca wersja

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
 
 @Component({
   selector: 'app-root',
@@ -16,6 +22,6 @@ import { Component } from '@angular/core';
   
 })
 
-export class DemoComponent {
+export class AppComponent {
   tablica: string[] = ['1', '2', '3'];
 }
