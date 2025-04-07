1634f9920c8fc2c1408e8cf62fbb02058dd9d517
bag3lk <czemujalol@outlook.com>
2025-04-07 11:45:56 +0200

    m

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'nowe' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('nowe');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
